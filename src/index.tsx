43e7a9925d2b18607163dc40c336299677f1e72c
Aleksi Hietanen <aleksi@identio.fi>
2019-11-05 15:07:05 +0200

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './domain/app/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const AppWrapper: React.FC = () => {
+    return (<Router><Switch><App /></Switch></Router>);
+};
+
+ReactDOM.render(<AppWrapper />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
