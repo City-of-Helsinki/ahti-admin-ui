0ec32ca0e7cd540e829dfbfdb0afb75811d33ab0
Aleksi Hietanen <aleksi@identio.fi>
2019-11-07 15:34:11 +0200

    Fix test, simplify index.ts

--- src/domain/app/App.tsx
+++ src/domain/app/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styles from './app.module.scss';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 const App: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <Route exact path="/">
-      <div className={styles.app}>
-        <header className={styles.header}>
-          <p>{t('home.title')}</p>
-        </header>
-      </div>
-    </Route>
+    <Router>
+      <Route exact path="/">
+        <div className={styles.app}>
+          <header className={styles.header}>
+            <p>{t('home.title')}</p>
+          </header>
+        </div>
+      </Route>
+    </Router>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './domain/app/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import './domain/i18n/i18n';
 import './assets/styles/main.scss';
 
-const AppWrapper: React.FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <App />
-      </Switch>
-    </Router>
-  );
-};
-
-ReactDOM.render(<AppWrapper />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
